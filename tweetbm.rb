2595f4fda3e881695ee5e89129f2a35999774e13
Nacamura <t.nacamura@gmail.com>
2014-04-02 23:41:03 +0900

    remove path from load resource

--- tweetbm.rb
+++ tweetbm.rb
@@ -1,21 +1,21 @@
 require 'json'
-load './tweetbookmark/mylogger.rb'
-load './tweetbookmark/mytwitter.rb'
-load './tweetbookmark/instapaper.rb'
-load './tweetbookmark/pocket.rb'
+load 'mylogger.rb'
+load 'mytwitter.rb'
+load 'instapaper.rb'
+load 'pocket.rb'
 
 class TweetBookMark
   include MyLogger
   def call
-    settings = load_json("./tweetbookmark/settings.txt")
-    stored_urls = load_json("./tweetbookmark/urls.txt")
+    settings = load_json("settings.txt")
+    stored_urls = load_json("urls.txt")
     twitter = MyTwitter.new(settings)
     ng_words = settings["ng_words"]
     urls = twitter.gather_hatebu_urls_without_ng(ng_words).reverse!
     new_urls = urls.reject {|url| stored_urls.include? url}
     Instapaper.new(settings).add_all(new_urls)
     Pocket.new(settings).add_all(new_urls)
-    store_json(urls, "./tweetbookmark/urls.txt")
+    store_json(urls, "urls.txt")
   end
 
   def load_json(textfile_path)
