ba21499e33b1b9571e2d394f2ac4c0f9f7c47c96
Nacamura <t.nacamura@gmail.com>
2014-04-15 23:11:43 +0900

    remove unused methods

--- mylogger.rb
+++
@@ -1,8 +0,0 @@
-require 'logger'
-
-module MyLogger
-	@@logger
-	def get_logger
-		@@logger ||= Logger.new('./log.txt')
-	end
-end
--- tweetbm.rb
+++ tweetbm.rb
@@ -7,27 +7,15 @@ load 'pocket.rb'
 class TweetBookMark
   include MyLogger
   def call
-    settings = load_json("settings.txt")
-    stored_urls = load_json("urls.txt")
+    settings = MyJSON.load_json("settings.txt")
+    stored_urls = MyJSON.load_json("urls.txt")
     twitter = MyTwitter.new(settings)
-    ng_words = load_json("ng_words.txt")
+    ng_words = MyJSON.load_json("ng_words.txt")
     urls = twitter.gather_hatebu_urls_without_ng(ng_words).reverse!
     new_urls = urls.reject {|url| stored_urls.include? url}
     Instapaper.new(settings).add_all(new_urls)
     Pocket.new(settings).add_all(new_urls)
-    store_json(urls, "urls.txt")
+    MyJSON.store_json(urls, "urls.txt")
     twitter.dump_recorded
-  end
-
-  def load_json(textfile_path)
-    open(textfile_path) do |io|
-      JSON.load(io)
-    end
-  end
-
-  def store_json(array, textfile_path)
-    open(textfile_path, "w") do |io|
-      JSON.dump(array, io)
-    end
   end
 end
